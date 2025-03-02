cc032c22681284589dc440bb4a3373d292e88cd2
Lara Langnau <laralangnau+github@gmail.com>
2025-03-02 18:52:47 +0100

    add DungeonTile and change Dungeon

--- 3-domain/src/main/java/com/example/domain/Dungeon.java
+++ 3-domain/src/main/java/com/example/domain/Dungeon.java
@@ -1,37 +1,42 @@
 package com.example.domain;
 
 
+import com.example.domain.map.DungeonTile;
 
+import java.util.List;
 
 public class Dungeon {
-    private int level;
-    private int difficulty;
-    private int monsterCount;
-    private int itemCount;
-    private Map map;
-    public Dungeon(int level, int difficulty, int monsterCount, int itemCount, Map map) {
-        this.level = level;
-        this.difficulty = difficulty;
-        this.monsterCount = monsterCount;
-        this.itemCount = itemCount;
-        this.map = map;
-//        for(int i=0; i<monsterCount; i++) {
-//            Random rnd = new Random();
-//            int x = rnd.nextInt(grid.length);
-//            int y = rnd.nextInt(grid[0].length);
-//            if(grid[x][y].getTileType() == TileType.NOTHING) {
-//                this.grid[x][y] = MonsterFactory.createMonsterRandom();
-//            }
-//        }
-//        for(int i=0; i<monsterCount; i++) {
-//            Random rnd = new Random();
-//            int x = rnd.nextInt(grid.length);
-//            int y = rnd.nextInt(grid[0].length);
-//            if(grid[x][y].getTileType() == TileType.NOTHING) {
-//                this.grid[x][y] = MonsterFactory.createMonsterRandom();
-//            }
-//        }
+//    private int level;
+//    private int difficulty;
+    private int enemyAmount;
+    private int remainingEnemies;
+    private int itemAmount;
+    private DungeonTile[][] dungeonTiles;
+    private int width;
+    private int height;
+    private List<LivingEntity> entities;
+    private Position playerSpawnPoint;
 
+// TODO add items, level and difficulty
+    public Dungeon(int enemyAmount, DungeonTile[][] dungeonTiles, int width, int height, List<LivingEntity> entities, Position playerSpawnPoint) {
+//        this.level = level;
+//        this.difficulty = difficulty;
+        this.enemyAmount = enemyAmount;
+        this.remainingEnemies = enemyAmount;
+//        this.itemAmount = itemAmount;
+        this.dungeonTiles = dungeonTiles;
+        this.width = width;
+        this.height = height;
+        this.entities = entities;
+        this.playerSpawnPoint = playerSpawnPoint;
     }
 
+    public Position getPlayerSpawnPoint() {
+        return playerSpawnPoint;
+    }
+
+    public int getRemainingEnemies() {
+        //TODO checken ob remaining enemies ein eigener count sein sollte oder einfach aus der Liste der Entities berechnet wird
+        return remainingEnemies;
+    }
 }
---
+++ 3-domain/src/main/java/com/example/domain/map/DungeonTile.java
@@ -0,0 +1,25 @@
+package com.example.domain.map;
+
+import java.awt.*;
+
+public enum DungeonTile {
+    Floor('.', Color.white, Color.LIGHT_GRAY, true),
+    Wall('#', Color.yellow, Color.orange, false),
+    Bounds('x', Color.black, Color.black, false),
+    Unknown(' ', Color.black, Color.black, true);
+
+    private final char displayCharacter;
+
+    private final Color primaryColor;
+    private final Color secondaryColor;
+    private final boolean walkable;
+    private final boolean blocksSight;
+
+    DungeonTile(char displayCharacter, Color primaryColor, Color secondaryColor, boolean walkable) {
+        this.displayCharacter = displayCharacter;
+        this.primaryColor = primaryColor;
+        this.secondaryColor = secondaryColor;
+        this.walkable = walkable;
+        this.blocksSight = !walkable;
+    }
+}
