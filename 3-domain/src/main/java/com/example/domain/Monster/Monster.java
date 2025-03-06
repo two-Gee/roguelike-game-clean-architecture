85b4343c9864ba112926775562fb783573137ba6
Gerrit <gerrit.graetz@gmail.com>
2025-03-06 15:06:00 +0100

    added UUID to Monster

--- 3-domain/src/main/java/com/example/domain/Monster/Monster.java
+++ 3-domain/src/main/java/com/example/domain/Monster/Monster.java
@@ -1,51 +1,64 @@
 package com.example.domain.Monster;
 
 
-import com.example.domain.Direction;
-import com.example.domain.Entity;
-import com.example.domain.Position;
+import com.example.domain.*;
 
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 
-public class Monster extends Entity {
+public class Monster extends LivingEntity {
     private String name;
-    private int health;
-    private int damage;
-    private Position position;
-    private int speed;
+    private UUID id;
 
-    public Monster(String name, int health, int damage, int speed) {
-        super();
+
+    public Monster(String name, int health, int damage) {
+        super(health, damage);
         this.name = name;
-        this.health = health;
-        this.damage = damage;
-        this.speed = speed;
+        this.id = UUID.randomUUID();
     }
 
     public String getName() {
         return name;
     }
 
-    public int getHealth() {
-        return health;
-    }
 
-    public int getDamage() {
-        return damage;
-    }
+//    public void moveRandom(){
+//        Random rnd = new Random();
+//        int randomNumber = rnd.nextInt(4);
+//        super.move(Direction.values()[randomNumber]);
+//    }
 
-    public void takeDamage(int damage) {
-        health -= damage;
-    }
-
-    public boolean isDead() {
-        return health <= 0;
-    }
-
-    public void moveRandom(){
-        Random rnd = new Random();
-        int randomNumber = rnd.nextInt(4);
-        super.move(Direction.values()[randomNumber]);
-    }
+//    public void moveTowardPlayer(Player target, List<Monster> otherMonsters){
+//        Position playerPos = target.getPosition();
+//        Position monsterPos = this.getPosition();
+//        int xDiff = playerPos.getxPos() - monsterPos.getxPos();
+//        int yDiff = playerPos.getyPos() - monsterPos.getyPos();
+//        if (Math.abs(xDiff) == 1 && Math.abs(yDiff) == 1) {
+//            this.attack(target);
+//        }else {
+//            Position newPos;
+//            if (Math.abs(xDiff) > Math.abs(yDiff)){
+//                if (xDiff > 0){
+//                    newPos = new Position(monsterPos.getxPos() + 1, monsterPos.getyPos());
+//                } else {
+//                    newPos = new Position(monsterPos.getxPos() - 1, monsterPos.getyPos());
+//                }
+//            } else {
+//                if (yDiff > 0){
+//                    newPos = new Position(monsterPos.getxPos(), monsterPos.getyPos() + 1);
+//                } else {
+//                    newPos = new Position(monsterPos.getxPos(), monsterPos.getyPos() - 1);
+//                }
+//            }
+//            for(Monster monster : otherMonsters){
+//                if (monster.getPosition().equals(newPos)){
+//                    return;
+//                }
+//            }
+//            this.setPosition(newPos);
+//
+//        }
+//    }
 }
