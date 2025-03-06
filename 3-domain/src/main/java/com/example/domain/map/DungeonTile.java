b751b83d422bdffd6f608302c1902df77ad9c10a
Lara Langnau <laralangnau+github@gmail.com>
2025-03-06 13:55:42 +0100

    change displayCharacter for DungeonTile

--- 3-domain/src/main/java/com/example/domain/map/DungeonTile.java
+++ 3-domain/src/main/java/com/example/domain/map/DungeonTile.java
@@ -3,23 +3,33 @@ package com.example.domain.map;
 import java.awt.*;
 
 public enum DungeonTile {
-    Floor('.', Color.white, Color.LIGHT_GRAY, true),
-    Wall('#', Color.yellow, Color.orange, false),
-    Bounds('x', Color.black, Color.black, false),
-    Unknown(' ', Color.black, Color.black, true);
+    Floor(" . ", Color.white, Color.LIGHT_GRAY, true),
+    Wall("███", Color.yellow, Color.orange, false),
+    Bounds("xxx", Color.black, Color.black, false),
+    Unknown("   ", Color.black, Color.black, true);
 
-    private final char displayCharacter;
+    private final String displayCharacter;
 
     private final Color primaryColor;
     private final Color secondaryColor;
     private final boolean walkable;
     private final boolean blocksSight;
 
-    DungeonTile(char displayCharacter, Color primaryColor, Color secondaryColor, boolean walkable) {
+    DungeonTile(String displayCharacter, Color primaryColor, Color secondaryColor, boolean walkable) {
         this.displayCharacter = displayCharacter;
         this.primaryColor = primaryColor;
         this.secondaryColor = secondaryColor;
         this.walkable = walkable;
         this.blocksSight = !walkable;
     }
+
+    public Color getColour(TileColorType tileColorType) {
+        return tileColorType == TileColorType.Primary ? primaryColor : secondaryColor;
+    }
+
+    public String getDisplayCharacter() {
+        return displayCharacter;
+    }
+
+    public enum TileColorType { Primary, Secondary }
 }
