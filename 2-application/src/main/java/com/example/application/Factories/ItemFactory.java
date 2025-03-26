424fc5e76c3397026244f18934d521778826b5e5
Gerrit <gerrit.graetz@gmail.com>
2025-03-26 16:27:47 +0100

    Fix: no overlapping items

--- 2-application/src/main/java/com/example/application/Factories/ItemFactory.java
+++ 2-application/src/main/java/com/example/application/Factories/ItemFactory.java
@@ -19,10 +19,17 @@ public class ItemFactory {
         };
     }
 
-    public static Item createRandomItem(DungeonRoom room) {
-        int x = (int) (Math.random() * (room.getBottomRightCorner().getxPos() - room.getTopLeftCorner().getxPos()) + room.getTopLeftCorner().getxPos());
-        int y = (int) (Math.random() * (room.getBottomRightCorner().getyPos() - room.getTopLeftCorner().getyPos()) + room.getTopLeftCorner().getyPos());
-        Position position = new Position(x, y);
+    public static Item createRandomItem(DungeonRoom room, List<Item> existingItems) {
+        Position position;
+        boolean positionOccupied;
+
+        do {
+            int x = (int) (Math.random() * (room.getBottomRightCorner().getxPos() - room.getTopLeftCorner().getxPos()) + room.getTopLeftCorner().getxPos());
+            int y = (int) (Math.random() * (room.getBottomRightCorner().getyPos() - room.getTopLeftCorner().getyPos()) + room.getTopLeftCorner().getyPos());
+            position = new Position(x, y);
+            final Position finalPosition = position;
+            positionOccupied = existingItems.stream().anyMatch(item -> item.getPosition().equals(finalPosition));
+        } while (positionOccupied);
 
         int random = (int) (Math.random() * ItemTypes.values().length);
         return createItem(ItemTypes.values()[random], position, room.getRoomNumber());
@@ -32,7 +39,7 @@ public class ItemFactory {
         List<Item> items = new ArrayList<>();
 
         for (int i = 0; i < amount; i++) {
-            items.add(createRandomItem(room));
+            items.add(createRandomItem(room, items));
         }
         return items;
     }
